4299d4f94c4670f7550fd526f12a4528625b521b
zhangxiaode <2279323443@qq.com>
2021-05-28 17:56:39 +0800

    引入ts,使用web性能监控

---
+++ src/views/app.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './app.scss';
+class App extends React.Component {
+  constructor(props: any) {
+    super(props)
+    this.state = { 
+
+    }
+  }
+  componentdidMounted() {
+
+  }
+  render() {
+    return (
+      <h1>Hello</h1>
+    )
+  }
+}
+
+export default App;
